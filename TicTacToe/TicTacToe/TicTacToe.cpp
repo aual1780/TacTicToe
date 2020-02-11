7dfc1c636522514fba8b7d145121a7fd92ec4c73
aual1780 <aual1780@colorado.edu>
2020-02-10 19:00:41 -0700

    Implement board render

--- TicTacToe/TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe/TicTacToe.cpp
@@ -1,25 +1,75 @@
 #include <iostream>
 #include <vector>
 
+#define UNDERLINE "\033[4m"
+#define CLOSEUNDERLINE "\033[0m"
+
 enum class BoardCell
 {
     Empty,
     X,
     O,
 };
+std::string CellToString(const BoardCell& cell)
+{
+    switch (cell)
+    {
+    case BoardCell::Empty:
+        return "_";
+    case BoardCell::X:
+        return "X";
+    case BoardCell::O:
+        return "O";
+    default:
+        return "_";
+    }
+}
+
+
+std::vector<std::vector<BoardCell>> CreateBoard(int rows, int cols)
+{
+    auto arr = std::vector<std::vector<BoardCell>>(rows, std::vector<BoardCell>(cols, BoardCell::Empty));
+    return arr;
+}
+
+
+void DisplayBoard(std::vector<std::vector<BoardCell>> arr, int rows, int cols)
+{
+    for (int r = 0; r < rows; r++)
+    {
+        for (int c = 0; c < cols; c++)
+        {
+            std::cout << CellToString(arr[r][c]);
+
+            if (c < cols - 1)
+            {
+                std::cout << "|";
+            }
+        }
+
+        if (r < rows - 1)
+        {
+            std::cout << std::endl;
+        }
+    }
+}
+
 
 int main()
 {
-    auto board = CreateBoard(3, 3);
+    int rows = 3;
+    int cols = 3;
+    auto board = CreateBoard(rows, cols);
+
+
+    DisplayBoard(board, rows, cols);
+
+
+    std::string discard;
+    std::cin >> discard;
 
     return 0;
 }
 
 
 
-std::vector<std::vector<BoardCell>> CreateBoard(int rows, int cols)
-{
-    auto arr = std::vector<std::vector<BoardCell>>(rows, std::vector<BoardCell>(cols, BoardCell::Empty));
-
-    return arr;
-}
