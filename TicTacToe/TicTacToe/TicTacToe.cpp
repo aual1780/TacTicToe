943d9f84e87ce8d919497d2b3d01aeafaf8779a5
aual1780 <aual1780@colorado.edu>
2020-02-10 18:41:00 -0700

    Implement CreateBoard

--- TicTacToe/TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe/TicTacToe.cpp
@@ -1,7 +1,25 @@
-#include <cstdio>
+#include <iostream>
+#include <vector>
+
+enum class BoardCell
+{
+    Empty,
+    X,
+    O,
+};
 
 int main()
 {
-    printf("hello from TicTacToe!\n");
+    auto board = CreateBoard(3, 3);
+
     return 0;
+}
+
+
+
+std::vector<std::vector<BoardCell>> CreateBoard(int rows, int cols)
+{
+    auto arr = std::vector<std::vector<BoardCell>>(rows, std::vector<BoardCell>(cols, BoardCell::Empty));
+
+    return arr;
 }
